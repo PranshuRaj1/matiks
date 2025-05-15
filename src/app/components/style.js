82326676c93e3671367f4032eac0280bea459295
Pranshu Raj <rajpranshu2222@gmail.com>
2025-05-15 15:53:39 +0530

    Matiks

---
+++ src/app/components/style.js
@@ -0,0 +1,30 @@
+// style = {
+//     "r-1d5kdc7" : "flex-direction: column-reverse"
+//     ,
+//     "r-1p0dtai" : "bottom: 0px;",
+//     "r-1d2f490" : "left: 0px;",
+//     "r-u8s1d" : " position: absolute;",
+//     "r-zchlnj" : "right: 0px;",
+//     "r-ipm5af" : "top: 0px;",
+//     "r-13awgt0" : "flex: 1 1 0%;"
+// }
+
+//  css-175oi2r =  {
+//     align-items: stretch;
+//     background-color: rgba(0, 0, 0, 0);
+//     border: 0px solid black;
+//     box-sizing: border-box;
+//     display: flex
+// ;
+//     flex-basis: auto;
+//     flex-direction: column;
+//     /* flex-shrink: 0; */
+//     list-style: none;
+//     margin: 0px;
+//     min-height: 0px;
+//     min-width: 0px;
+//     padding: 0px;
+//     position: relative;
+//     text-decoration: none;
+//     z-index: 0;
+// }
---
+++ tailwind.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+  darkMode: "class",
+  theme: {
+    extend: {
+      colors: {
+        black: {
+          0: "var(--c-black0)",
+          1: "var(--c-black1)",
+          2: "var(--c-black2)",
+          3: "var(--c-black3)",
+          4: "var(--c-black4)",
+          5: "var(--c-black5)",
+          6: "var(--c-black6)",
+          7: "var(--c-black7)",
+          8: "var(--c-black8)",
+          9: "var(--c-black9)",
+          10: "var(--c-black10)",
+          11: "var(--c-black11)",
+          12: "var(--c-black12)",
+        },
+        white: {
+          0: "var(--c-white0)",
+          25: "var(--c-white025)",
+          50: "var(--c-white05)",
+          75: "var(--c-white075)",
+        },
+        blue: {
+          4: "var(--c-blue4Dark)",
+          Light: "var(--c-blue4Light)",
+        },
+      },
+    },
+  },
+};
