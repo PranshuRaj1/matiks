b2339f271c6b55a4080db7d1b51720ba58776878
Pranshu Raj <rajpranshu2222@gmail.com>
2025-05-18 17:28:44 +0530

    linting

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,3 @@
-import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Download from "./components/Download";
@@ -7,12 +6,10 @@ import Footer from "./components/Footer";
 export default function Home() {
   return (
     <>
-      {/* <NavBar /> */}
       <Hero />
       <Features />
       <Download />
       <Footer />
-      {/* <div className="antialiased text-gray-800 min-h-screen flex flex-col"></div> */}
     </>
   );
 }
